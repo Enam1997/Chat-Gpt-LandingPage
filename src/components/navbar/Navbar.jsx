6431b916cb501130354433f5298d681953cd66e5
Enam <mdenamahmedchowdhury@gmail.com>
2023-01-14 15:16:58 +0600

    Navbar Completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
-import "./navbar.css"
+import React, { useState } from "react";
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home"></a>Home
+    </p>
+    <p>
+      <a href="#whatGPT">What is GPT</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studys</a>
+    </p>
+    <p>
+      <a href="#blog"></a>Librabry
+    </p>
+  </>
+);
 
 const Navbar = () => {
+  const [toggoleMenu, setToggleMenu] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="gpt__navbar">
+      <div className="gpt__navbar-links">
+        <div className="gpt__navbar-links_logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="gpt__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign Up</button>
+      </div>
 
-export default Navbar
+      <div className="gpt__navbar-menu">
+        {toggoleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+
+        {toggoleMenu && (
+          <div className="gpt__navbar-menu_container scale-up-center">
+            <div className="gpt__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt__navbar-menu_containe-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign Up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+// Import main design file
 import App from './App';
+
+// Import the Css File
+import './index.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
